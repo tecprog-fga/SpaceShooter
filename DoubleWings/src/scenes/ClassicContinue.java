88b5409f4c8405ebf449784bed5651c211498b7a
Adailson Santos <adailson2@gmail.com>
2018-08-30 19:31:39 -0300

    Initialize all variables when declaring
    Signed-off-by: Adailson Santos <adailson2@gmail.com>

--- DoubleWings/src/scenes/ClassicContinue.java
+++ DoubleWings/src/scenes/ClassicContinue.java
@@ -11,134 +11,140 @@ import util.CountDownTimer;
 import util.CountDownTimerEnds;
 
 public class ClassicContinue extends GameScene implements CountDownTimerEnds {
-		
-	
+
+
 	@Override
 	protected void viewSetup() {
 		// TODO Auto-generated method stub
 	}
-	
+
 	/**
 	 * Sprite for build first screen of game's continue
 	 */
-	private Sprite continueScreen; //T01 T05 T07
+	private Sprite continueScreen = null; //T01 T05 T07 T06
 	/**
 	 * This sprite build screen for countdown of game's continue
 	 */
-	private Sprite countdownScreen; //T01 T05 T07
+	private Sprite countdownScreen = null; //T01 T05 T07 T06
 	/**
 	 * This sprite build enter screen after the limit game's continue is done
 	 */
-	private Sprite enterScreen; //T01 T05 T07
-	
+	private Sprite enterScreen = null; //T01 T05 T07 T06
+
 	/**
 	 * This object represent the building delpth scene screen
 	 */
-	private Parallax delpthScene; //T01 T05 T07
-	
+	private Parallax delpthScene = null; //T01 T05 T07 T06
+
 	public void initialSetup() {
-		
+
 		//Configure enter key and escape
 		this.keyboard.setBehavior(Keyboard.ENTER_KEY, InputBase.DETECT_INITIAL_PRESS_ONLY);
 		this.keyboard.setBehavior(Keyboard.ESCAPE_KEY, InputBase.DETECT_INITIAL_PRESS_ONLY);
-		
+
 		//Creation a object to class Parallax
-        this.delpthScene = new Parallax();
-		
-        //The first one added will be the last one to be painted.
+		this.delpthScene = new Parallax();
+
+		//The first one added will be the last one to be painted.
 		this.delpthScene.add("src/assets/img/temp_background.png"); //$NON-NLS-1$
 		this.delpthScene.add("src/assets/img/universe1.png"); //$NON-NLS-1$
 		this.delpthScene.add("src/assets/img/universe2.jpg"); //$NON-NLS-1$
 		this.delpthScene.add("src/assets/img/universe3.jpg"); //$NON-NLS-1$
 		//Since universe4.jpg was the last to be added to the list, it will be the main layer (mainLayer).
 		this.delpthScene.add("src/assets/img/universe4.jpg"); //$NON-NLS-1$
-		
+
 		//Adjusts the speed of all layers from the main layer
 		this.delpthScene.setVelAllLayers(0, 1);
-		
+
 		/*Define scenes elements position
 		 *Continue sprite upper-center position*/
 		this.continueScreen = new Sprite("src/assets/img/continue/continue.png"); //$NON-NLS-1$
 		this.continueScreen.x = WindowConstants.WIDTH/2 - this.continueScreen.width/2;
 		this.continueScreen.y = WindowConstants.HEIGHT/2 - this.continueScreen.height;
-		
+
 		//Number sprite positions
 		this.countdownScreen = new Sprite("src/assets/img/continue/number_9.png"); //$NON-NLS-1$
 		this.countdownScreen.x = WindowConstants.WIDTH/2 - this.countdownScreen.width/2;
 		this.countdownScreen.y = WindowConstants.HEIGHT/1.5 - this.countdownScreen.height/2;
-		
+
 		//Enter Sprite displays Screen
 		this.enterScreen = new Sprite("src/assets/img/continue/Enter-Download-PNG.png"); //$NON-NLS-1$
 		this.enterScreen.x = WindowConstants.WIDTH/2 - this.enterScreen.width/2;
 		this.enterScreen.y = WindowConstants.HEIGHT/500 - this.enterScreen.height/20;
-		
+
 		timeWait();
 	}
-	
-	//Time wait to transit between scene
+
+	// Time wait to transit between scene
 	public void timeWait(){
-		
-		Timer timer = new Timer();
-		CountDownTimer countDownn = new CountDownTimer();
-		countDownn.delegate = this;
-		long delay = 1000;
-		timer.scheduleAtFixedRate(countDownn, delay, delay);
-	
+
+		Timer timer = null; //T06
+		timer = new Timer();
+
+		CountDownTimer countDown = new CountDownTimer();
+		countDown.delegate = this;
+
+		final int delay = 1000; //T07 T08 
+		timer.scheduleAtFixedRate(countDown, delay, delay);
+
 	}
-	
-	//Update image Sprite on Screen and check button at keyboard
+
+	// Update image Sprite on Screen and check button at keyboard
 	public void update() {
-		
-		final int PIXELS_DOWN = 800; //T08
-		final int PIXELS_UP = 600;
-		
+
+		final int PIXELS_DOWN = 800; //T07 T08
+		final int PIXELS_UP = 600; //T07 T08
+
 		//Print all layers that have been added
-        this.delpthScene.drawLayers();
-		
-        //The method below is responsible for maintaining infinite repetition of the layers.
+		this.delpthScene.drawLayers();
+
+		//The method below is responsible for maintaining infinite repetition of the layers.
 		this.delpthScene.repeatLayers(PIXELS_DOWN, PIXELS_UP, false);
-		
+
 		//Move the parallax orientation vertically
 		this.delpthScene.moveLayersStandardY(false);
-		
+
 		this.continueScreen.draw();
 		this.countdownScreen.draw();
 		this.enterScreen.draw();
-		
+
 		checkButtonSelection();
-		
+
 	}
-	
-	//Method that after the end of the time transit to the screen of game over.
+
+	// Method that after the end of the time transit to the screen of game over.
 	@Override
 	public void terminate() {
 		if (this.game != null){
 			System.out.println("Timer Ended"); //$NON-NLS-1$
-			GameScene gameOver = new GameOver();
+			GameScene gameOver = null; //T06 T07 T08
+			gameOver = new GameOver();
 			this.game.transitTo(gameOver);
 		}
 	}
-	
-	//Update number continue on Screen
+
+	// Update number continue on Screen
 	@Override
 	public void updateImageForIndex(int index) {
 		this.countdownScreen.loadImage("src/assets/img/continue/number_" + String.valueOf(index) + ".png");  //$NON-NLS-1$//$NON-NLS-2$
 	}
-	
-	//Method to catch click on keyboard
+
+	// Method to catch click on keyboard
 	private void checkButtonSelection () {
-		
+
 		if(this.game != null && this.keyboard != null){
 			if (this.keyboard.keyDown(Keyboard.ENTER_KEY)) {
 				//Transit to a continue state of the game
-				MenuScene transitScene = new MenuScene();
+				MenuScene transitScene = null; //T06 T07 T08
+				transitScene = new MenuScene();
 				this.game.transitTo(transitScene.firstStage());
-	
+
 			} else if (this.keyboard.keyDown(Keyboard.ESCAPE_KEY)) {
-				GameScene menu = new MenuScene();
+				GameScene menu = null; //T06 T07 T08
+				menu = new MenuScene();
 				this.game.transitTo(menu);
 			}
 		}
 	}
-	
+
 }
--- DoubleWings/src/scenes/GameOver.java
+++ DoubleWings/src/scenes/GameOver.java
@@ -11,8 +11,8 @@ import util.CountDownTimerEnds;
 public class GameOver extends GameScene implements CountDownTimerEnds {
 
 	// Sprite on scene
-	private GameImage background;
-	private Sprite gameOver;
+	private GameImage background = null; //T06
+	private Sprite gameOver = null; //T06
 
 	public void initialSetup() {
 
@@ -24,6 +24,7 @@ public class GameOver extends GameScene implements CountDownTimerEnds {
 
 		//Creation image Game Over
 		this.gameOver = new Sprite("src/assets/img/continue/3540295891_logo.jpg"); //$NON-NLS-1$
+		
 		//Game over sprite center position
 		this.gameOver.x = WindowConstants.WIDTH/2 - this.gameOver.width/2;
 		this.gameOver.y = WindowConstants.HEIGHT/2 - this.gameOver.height/2;
@@ -39,26 +40,32 @@ public class GameOver extends GameScene implements CountDownTimerEnds {
 
 	//Time wait to transit between scene
 	public void timeWait(){
-		Timer timer = new Timer();
-		CountDownTimer countDownn = new CountDownTimer();
-		countDownn.delegate = this;
-		long delay = 1000;
-		timer.scheduleAtFixedRate(countDownn, delay, delay);
+		
+		Timer timer = null;
+		timer = new Timer();
+		
+		CountDownTimer countDown = null;
+		countDown = new CountDownTimer();
+		countDown.delegate = this;
+		
+		final int delay = 1000;
+		timer.scheduleAtFixedRate(countDown, delay, delay);
 	}
 
 	//Update image Sprite on Screen
 	public void update() {
-
 		this.background.draw();
 		this.gameOver.draw();
-
 	}
 
 	//Method to return to main Menu
 	@Override
 	public void terminate() {
 		System.out.println("Timer Ended"); //$NON-NLS-1$
-		GameScene menu = new MenuScene();
+		
+		GameScene menu = null;
+		menu = new MenuScene();
+		
 		this.game.transitTo(menu);
 	}
 
