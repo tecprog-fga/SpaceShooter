b5862f5d87a75179de4b8f27ad74c37536fe43fc
Adailson Santos <adailson2@gmail.com>
2018-08-30 18:58:25 -0300

    Resolving warnings in GameOver
    Signed-off-by: Adailson Santos <adailson2@gmail.com>

--- DoubleWings/src/scenes/GameOver.java
+++ DoubleWings/src/scenes/GameOver.java
@@ -17,16 +17,16 @@ public class GameOver extends GameScene implements CountDownTimerEnds {
 	public void initialSetup() {
 		
 		//Set game controller elements
-		keyboard = game.keyboard;
+		this.keyboard = this.game.keyboard;
 		
 		//Creation image background
-		background = new GameImage("src/assets/img/temp_background.png");
+		this.background = new GameImage("src/assets/img/temp_background.png"); //$NON-NLS-1$
 	
 		//Creation image Game Over
-		gameOver = new Sprite("src/assets/img/continue/3540295891_logo.jpg");
+		this.gameOver = new Sprite("src/assets/img/continue/3540295891_logo.jpg"); //$NON-NLS-1$
 		//Game over sprite center position
-		gameOver.x = WindowConstants.WIDTH/2 - gameOver.width/2;
-		gameOver.y = WindowConstants.HEIGHT/2 - gameOver.height/2;
+		this.gameOver.x = WindowConstants.WIDTH/2 - this.gameOver.width/2;
+		this.gameOver.y = WindowConstants.HEIGHT/2 - this.gameOver.height/2;
 		
 		timeWait();
 	}
@@ -50,17 +50,17 @@ public class GameOver extends GameScene implements CountDownTimerEnds {
 	//Update image Sprite on Screen
 	public void update() {
 		
-		background.draw();
-		gameOver.draw();
+		this.background.draw();
+		this.gameOver.draw();
 		
 	}
 	
 	//Method to return to main Menu
 	@Override
 	public void terminate() {
-		System.out.println("Timer Ended");
+		System.out.println("Timer Ended"); //$NON-NLS-1$
 		GameScene menu = new MenuScene();
-		game.transitTo(menu);
+		this.game.transitTo(menu);
 	}
 	
 	@Override
