b8248c5a51cd3246ef1e560c29e36dd35ea99da7
Isaac Borges <isaacbmota@gmail.com>
2018-09-03 00:32:49 -0300

    Applying Style and Design #3

--- DoubleWings/src/util/DelayTimer.java
+++ DoubleWings/src/util/DelayTimer.java
@@ -3,40 +3,44 @@ package util;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class DelayTimer extends Timer implements DelayDelegate{
+public class DelayTimer extends Timer implements DelayDelegate {
 
 	private DelayDelegate delegate;
 	private int type = 0;
 
-	public DelayTimer(DelayDelegate delegate, int type){
+	public DelayTimer(DelayDelegate delegate, int type) {
+		
 		this.type = type;
 		this.delegate = delegate;
 	}
 
-	public void schedule(long delay){
+	public void schedule(long delay) {
+		
 		Task task = new Task(this);
 		this.schedule(task, delay);
 	}
 
 	@Override
 	public void delayEnded(DelayTimer timer) {
+		
 		delegate.delayEnded(this);
 	}
 
-	public int getType(){
+	public int getType() {
+		
 		return type;
 	}
 
-
-	private class Task extends TimerTask{
+	private class Task extends TimerTask {
 
 		private DelayDelegate delegate;
 
-		public Task(DelayDelegate delegate){
+		public Task(DelayDelegate delegate) {
 			this.delegate = delegate;
 		}
 
-		public void run(){
+		public void run() {
+			
 			delegate.delayEnded(null);
 		}
 	}
