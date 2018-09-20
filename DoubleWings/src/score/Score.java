fb0d61a77bff68b44cfe8fcc10e44859e798f3eb
Adailson Santos <adailson2@gmail.com>
2018-09-20 19:59:04 -0300

    Comment methods of score package #15
    Signed-off-by: Adailson Santos <adailson2@gmail.com>

--- DoubleWings/src/score/Score.java
+++ DoubleWings/src/score/Score.java
@@ -7,10 +7,18 @@ package score;
 public class Score {
 	private int count = 0;
 
+	/**
+	 * This method realize the increment of score for to count all score types
+	 * @param score
+	 */
 	public void increaseScore(ScoreType score) {
 		this.count += score.getValue();
 	}
 
+	/**
+	 * This method is used for get the result of count of scores
+	 * @return a integer with the amount count of scores
+	 */
 	public int getCount() {
 		return this.count;
 	}
--- DoubleWings/src/score/ScoreType.java
+++ DoubleWings/src/score/ScoreType.java
@@ -11,10 +11,18 @@ public enum ScoreType {
 
 	int scoreValue = 0;
 
+	/**
+	 * This constructor initialize the score value
+	 * @param scoreValue
+	 */
 	ScoreType(int scoreValue) {
 		this.scoreValue = scoreValue;
 	}
 
+	/**
+	 * This method is used for get the type of scores
+	 * @return a integer with the value of type of scores
+	 */
 	public int getValue() {
 		return this.scoreValue;
 	}
