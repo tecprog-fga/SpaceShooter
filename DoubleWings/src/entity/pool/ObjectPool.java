71bba0372be898de915b11e22bda287a5b0ca722
Victor Rodrigues <manovictor5@gmail.com>
2018-10-24 23:06:58 -0300

    #36 #34 #30 Removing duplicated type declaration and adding reference this

--- DoubleWings/src/entity/pool/ObjectPool.java
+++ DoubleWings/src/entity/pool/ObjectPool.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 public abstract class ObjectPool<Type> {
 	
 	// Stores the objects, like bullets and enemies
-	private ArrayList<Type> objects = new ArrayList<Type>();
+	private ArrayList<Type> objects = new ArrayList<>();
 	
 	/**
 	 * Release the objects in the screen when it's empty
@@ -26,14 +26,14 @@ public abstract class ObjectPool<Type> {
 		/*
 		 * Creation of objects when the screen is empty
 		 */
-		if (objects.isEmpty()){
+		if (this.objects.isEmpty()){
 			obj = create();
 		}else{
-			int last = objects.size() -1;
-			obj = objects.remove(last);
+			int last = this.objects.size() -1;
+			obj = this.objects.remove(last);
 		}
 		
-		assert(obj != null): "obj is returning null";
+		assert(obj != null): "obj is returning null"; //$NON-NLS-1$
 		return obj;
 	}
 	
@@ -46,8 +46,8 @@ public abstract class ObjectPool<Type> {
 	 */
 	public void acquire(Type obj){
 		
-		if (objects.contains(obj) == false){
-			objects.add(obj);
+		if (this.objects.contains(obj) == false){
+			this.objects.add(obj);
 		}
 	}
 }
